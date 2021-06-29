b0aba97af0d12f993c65a46f02b30a480c5151cd
Mateus Torres <mateustorres67@hotmail.com>
2021-06-28 23:16:11 -0300

    Ajustes no Mediator

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,16 +11,10 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            this._mediator = mediator;
-        }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities(){
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
