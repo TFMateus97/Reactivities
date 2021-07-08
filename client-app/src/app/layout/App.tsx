5424b15bf4e1c6e43c5ce6b49e397817fcd7b8e8
Mateus Torres <mateustorres67@hotmail.com>
2021-07-07 23:03:07 -0300

    Inicio da criação do dashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Container, Header, List } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -16,16 +17,12 @@ function App() {
   }, []);
 
   return (
-    <Fragment>
+    <>
       <NavBar></NavBar>
       <Container style={{ marginTop: "7em" }}>
-        <List>
-          {activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
+        <ActivityDashboard activities={activities}></ActivityDashboard>
       </Container>
-    </Fragment>
+    </>
   );
 }
 
