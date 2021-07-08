eb2d27a28859c1788009728820bb661e5443892d
Mateus Torres <mateustorres67@hotmail.com>
2021-07-07 23:59:50 -0300

    Criaçõ de card

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, List } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
 import ActivityList from "./ActivityList";
 
 interface Props {
@@ -10,10 +11,16 @@ interface Props {
 export default function ActivityDashboard({ activities }: Props) {
   //especificando que está pegando activities de Props
   return (
+    //o total de colunas possível aqui são 16
     <Grid>
       <Grid.Column width="10">
         <ActivityList activities={activities}></ActivityList>
       </Grid.Column>
+      <Grid.Column width="6">
+        {activities[0] && (
+          <ActivityDetails activity={activities[0]}></ActivityDetails> //é uma forma de garantir que o componente só vai ser carregado depois que ele existir
+        )}
+      </Grid.Column>
     </Grid>
-  );
+  ); //Atualmente vai ficar "hard coded" sempre pegando o primeiro, será removido posteriormente
 }
