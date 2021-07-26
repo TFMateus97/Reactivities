1a1fc99976feb20c453e11601047579100eae4e6
Mateus Torres <mateustorres67@hotmail.com>
2021-07-25 23:43:53 -0300

    Adicionando rotas

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
-import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
+import HomePage from "../../features/home/homePage";
+import { Route } from "react-router-dom";
+import ActivityForm from "../../features/activities/form/ActivityForm";
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loading App"></LoadingComponent>;
-
   return (
     <>
       <NavBar></NavBar>
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard></ActivityDashboard>
+        {/*  Garantir que o nome da rota seja apenas o informado no path */}
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/activities" component={ActivityDashboard}></Route>
+        <Route path="/createActivity" component={ActivityForm}></Route>
       </Container>
     </>
   );
