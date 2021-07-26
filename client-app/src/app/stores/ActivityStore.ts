9c034ac169625fdcaa62a76195e1c7efc7914352
Mateus Torres <mateustorres67@hotmail.com>
2021-07-25 23:31:52 -0300

    insstalação e adição do react router

--- client-app/src/app/stores/ActivityStore.ts
+++ client-app/src/app/stores/ActivityStore.ts
@@ -99,7 +99,7 @@ export default class ActivityStore{
             await agent.Activities.delete(id);
             runInAction(() => {
                 this.activityRegistry.delete(id);
-                if(this.selectedActivity?.id == id) this.cancelSelectedActivity();
+                if(this.selectedActivity?.id === id) this.cancelSelectedActivity();
                 this.loading = false;
             })
         } catch (error) {
