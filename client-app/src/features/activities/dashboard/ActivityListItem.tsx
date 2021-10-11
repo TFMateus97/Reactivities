78d61fdabacb749a31c17ff4392b1d1d00518399
Mateus Torres <mateustorres67@hotmail.com>
2021-10-10 21:23:13 -0300

    ajustes de layout

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,6 @@
 import React, { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Item, Label } from "semantic-ui-react";
+import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
 
@@ -23,36 +23,43 @@ export default function ActivityListItem({ activity }: Props) {
   }
 
   return (
-    <Item key={activity.id}>
-      <Item.Content>
-        <Item.Header as="a">{activity.title}</Item.Header>
-        <Item.Meta>{activity.date}</Item.Meta>
-        <Item.Description>
-          <div>{activity.description}</div>
-          <div>
-            {activity.city}, {activity.venue}
-          </div>
-        </Item.Description>
-        <Item.Extra>
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Image
+              size="tiny"
+              circular
+              src="/assets/user.png"
+            ></Item.Image>
+            <Item.Content>
+              <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                {activity.title}
+              </Item.Header>
+              <Item.Description>Hosted by Bobs</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <span>
+          <Icon name="clock" /> {activity.date}
+          <Icon name="marker" /> {activity.venue}
+        </span>
+      </Segment>
+      <Segment secondary>Attendees go here</Segment>
+      <Segment clearing>
+        <span>
+          {activity.description}
           <Button
             as={Link}
             to={`/activities/${activity.id}`}
+            color="teal"
             floated="right"
             content="View"
-            color="blue"
           ></Button>
-          {/* arrow function caso contrario o componente tentaria executar (activity.id) assim que fosse renderizado */}
-          <Button
-            onClick={(event) => handleActivityDelete(event, activity.id)}
-            floated="right"
-            content="Delete"
-            color="red"
-            loading={loading && target === activity.id}
-            name={activity.id}
-          ></Button>
-          <Label basic content={activity.category}></Label>
-        </Item.Extra>
-      </Item.Content>
-    </Item>
+        </span>
+      </Segment>
+    </Segment.Group>
   );
 }
