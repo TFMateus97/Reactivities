9ca056756893d09ae4f33e1866435756c94ccf42
Mateus Torres <mateustorres67@hotmail.com>
2021-11-01 00:20:53 -0300

    Criação de componentes de input e textarea reutilizaveis

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,11 +1,14 @@
 import { observer } from "mobx-react-lite";
 import React, { ChangeEvent, useEffect, useState } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
-import { Segment, Button } from "semantic-ui-react";
+import { Segment, Button, Label } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
 import { v4 as uuid } from "uuid";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import MyTextInput from "../../../app/common/form/MyTextInput";
+import MyTextArea from "../../../app/common/form/MyTextArea";
 
 export default observer(function ActivityForm() {
   const { activityStore } = useStore();
@@ -27,6 +30,15 @@ export default observer(function ActivityForm() {
     date: "",
     city: "",
     venue: "",
+  });
+
+  const validationSchema = Yup.object({
+    title: Yup.string().required("The acitivity title is required"),
+    description: Yup.string().required("The acitivity description is required"),
+    category: Yup.string().required(),
+    date: Yup.string().required(),
+    venue: Yup.string().required(),
+    city: Yup.string().required(),
   });
 
   useEffect(() => {
@@ -65,18 +77,23 @@ export default observer(function ActivityForm() {
   return (
     <Segment clearing>
       <Formik
+        validationSchema={validationSchema}
         enableReinitialize
         initialValues={activity}
         onSubmit={(values) => console.log(values)}
       >
         {({ values: activity, handleChange, handleSubmit }) => (
           <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
-            <Field placeholder="Title" name="title"></Field>
-            <Field placeholder="Description" name="description"></Field>
-            <Field placeholder="Category" name="category"></Field>
-            <Field type="Date" placeholder="Date" name="date"></Field>
-            <Field placeholder="City" name="city"></Field>
-            <Field placeholder="Venue" name="venue"></Field>
+            <MyTextInput name="title" placeholder="title"></MyTextInput>
+            <MyTextArea
+              rows={3}
+              placeholder="Description"
+              name="description"
+            ></MyTextArea>
+            <MyTextInput placeholder="Category" name="category"></MyTextInput>
+            <MyTextInput placeholder="Date" name="date"></MyTextInput>
+            <MyTextInput placeholder="City" name="city"></MyTextInput>
+            <MyTextInput placeholder="Venue" name="venue"></MyTextInput>
             <Button
               floated="right"
               positive
