e53a9179c66cb9e982e580f364c4a4a2882fac10
Mateus Torres <mateustorres67@hotmail.com>
2021-12-26 00:01:11 -0300

    Criando interface de profile

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -2,6 +2,7 @@ import { createContext, useContext } from "react";
 import ActivityStore from "./ActivityStore";
 import CommomStore from "./commonStore";
 import ModalStore from "./modalStore";
+import ProfileStore from "./ProfileStore";
 import UserStore from "./UserStore";
 
 interface Store {
@@ -9,17 +10,19 @@ interface Store {
     commomStore: CommomStore;
     userStore: UserStore;
     modalStore: ModalStore;
+    profileStore: ProfileStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
     commomStore: new CommomStore(),
     userStore: new UserStore(),
-    modalStore: new ModalStore()
-}
+    modalStore: new ModalStore(),
+    profileStore: new ProfileStore(),
+};
 
 export const StoreContext = createContext(store);
 
-export function useStore(){
+export function useStore() {
     return useContext(StoreContext);
 }
--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
+import ProfileContent from "./ProfileContent";
+import ProfileHeader from "./ProfileHeader";
 
 export default function ProfilePage() {
-    return <h1>Profile</h1>;
+    return (
+        <Grid>
+            <Grid.Column width={16}>
+                <ProfileHeader></ProfileHeader>
+                <ProfileContent></ProfileContent>
+            </Grid.Column>
+        </Grid>
+    );
 }
