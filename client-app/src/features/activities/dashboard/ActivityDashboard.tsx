3f9990c8f5bcefdca8b3a7fcca7f7098208ed1fc
Mateus Torres <mateustorres67@hotmail.com>
2022-01-08 00:12:58 -0300

    Implementação do scroll inifinito

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,31 +1,57 @@
 import { observer } from "mobx-react-lite";
-import React, { useEffect } from "react";
-import { Grid } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroller";
+import { Grid, Loader } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { PagingParams } from "../../../app/models/pagination";
 import { useStore } from "../../../app/stores/store";
 import ActivityFilters from "./ActivityFilters";
 import ActivityList from "./ActivityList";
 
 export default observer(function ActivityDashboard() {
-  const { activityStore } = useStore();
-  const { loadActivities, activityRegistry } = activityStore;
+    const { activityStore } = useStore();
+    const { loadActivities, activityRegistry, setPagingParams, pagination } =
+        activityStore;
+    const [loadingNext, setLoadingNext] = useState(false);
 
-  useEffect(() => {
-    if (activityRegistry.size <= 0) loadActivities();
-  }, [activityRegistry.size, loadActivities]);
+    function handleGetNext() {
+        setLoadingNext(true);
+        setPagingParams(new PagingParams(pagination!.currentPage + 1));
+        loadActivities().then(() => setLoadingNext(false));
+    }
 
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loading Activities"></LoadingComponent>;
+    useEffect(() => {
+        if (activityRegistry.size <= 0) loadActivities();
+    }, [activityRegistry.size, loadActivities]);
 
-  return (
-    //o total de colunas possível aqui são 16
-    <Grid>
-      <Grid.Column width="10">
-        <ActivityList></ActivityList>
-      </Grid.Column>
-      <Grid.Column width="6">
-        <ActivityFilters></ActivityFilters>
-      </Grid.Column>
-    </Grid>
-  );
+    if (activityStore.loadingInitial && !loadingNext)
+        return (
+            <LoadingComponent content="Loading Activities"></LoadingComponent>
+        );
+
+    return (
+        //o total de colunas possível aqui são 16
+        <Grid>
+            <Grid.Column width="10">
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={handleGetNext}
+                    hasMore={
+                        !loadingNext &&
+                        !!pagination &&
+                        pagination.currentPage < pagination.totalPages
+                    }
+                    initialLoad={false}
+                >
+                    <ActivityList></ActivityList>
+                </InfiniteScroll>
+            </Grid.Column>
+            <Grid.Column width="6">
+                <ActivityFilters></ActivityFilters>
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Loader active={loadingNext}></Loader>
+            </Grid.Column>
+        </Grid>
+    );
 });
